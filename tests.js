8d2174694851a54fe43262e2f30ef816112709be
Chip Bell <cbell@pbs.org>
2020-04-21 14:26:32 -0400

    Getting the actual test suite up and running

--- tests.js
+++ tests.js
@@ -1,7 +1,43 @@
 const { expect } = require('chai');
 
+const { GAME_SERVER_PORT } = require('./lib/constants.js');
+
 describe('test suite', () => {
-  it('should work', () => {
-    expect(1).to.equal(1);
+  let iframe;
+  let container;
+  let features;
+
+  before((done) => {
+    iframe = document.createElement('iframe');
+    iframe.id = 'game';
+    document.body.appendChild(iframe);
+
+    container = new springroll.Container('#game');
+    container.openPath(`http://localhost:${GAME_SERVER_PORT}`);
+
+    const start = Date.now();
+    container.on('features', (event) => {
+      console.log(`Features received in ${Date.now() - start}ms`);
+      features = event;
+      done();
+    });
   });
+
+  after(() => {
+    container.close();
+    document.body.removeChild(iframe);
+  });
+
+  const requiredFeatures = [
+    'sound',
+    'vo',
+    'sfx',
+    'music',
+  ];
+
+  for (const featureName of requiredFeatures) {
+    it(`should support ${featureName}`, async () => {
+      expect(features[featureName]).to.equal(true);
+    });
+  }
 });
