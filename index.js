f7e0e3c57e4ee20cec67e236b77a98bacf24a7b2
Chip Bell <cbell@pbs.org>
2020-04-21 10:21:29 -0400

    Initial command-line parsing with validation

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const packageJson = require('./package.json');
+const path = require('path');
+const { program } = require('commander');
+
+const expandPath = (rawPath) => {
+  // first, replace any home directory
+  const noTilde = rawPath.replace(/~/, process.env.HOME);
+
+  // Now, just resolve the path
+  return path.resolve(noTilde);
+};
+
+const validatePath = (providedPath) => {
+  const fullGamePath = expandPath(providedPath);
+  const indexHtmlPath = path.join(fullGamePath, 'index.html');
+
+  if (!fs.existsSync(fullGamePath)) {
+    console.error(`The path ${providedPath} (resolved to ${fullGamePath}) was not found`);
+  } else if(!fs.statSync(fullGamePath).isDirectory()) {
+    console.error(`The path ${providedPath} (resolved to ${fullGamePath}) is not a directory`);
+  } else if(!fs.existsSync(indexHtmlPath) || !fs.statSync(indexHtmlPath).isFile()) {
+    console.error(`The path ${providedPath} (resolved to ${fullGamePath}) did not contain an index.html`);
+  } else {
+    console.log(`${providedPath} valid`);
+  }
+};
+
+program.version(packageJson.version);
+program
+  .requiredOption('-s, --slug <slug>', 'The game slug to use when reporting test results')
+  .requiredOption('-p, --path <path>', 'The path to the deploy folder of the game'); 
+
+program.parse(process.argv);
+
+validatePath(program.path);
