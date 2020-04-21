9d51b0b439eb9386b2c6738b73814e317166862d
Chip Bell <cbell@pbs.org>
2020-04-21 11:36:15 -0400

    Loading plugins explicitly

--- karma.conf.js
+++ karma.conf.js
@@ -6,6 +6,13 @@ module.exports = function(config) {
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
+    
+    plugins: [
+      'karma-mocha',
+      'karma-webpack',
+      'karma-chrome-launcher',
+    ],
+
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
