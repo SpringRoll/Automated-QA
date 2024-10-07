516ba11637ea878b5fe6c0db2adb1c127c666eca
Rob Carter <rtcarter@pbs.org>
2024-10-07 11:24:16 -0400

    spacing on eslintconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,16 @@
 module.exports = {
-    env: {
-      node: true,
-      browser: true,
-      es6: true,
-      mocha: true,
-    },
-    extends: '@pbs/eslint-config-pbs-kids',
-    parserOptions: {
-      ecmaVersion: 2017,
-    },
-    rules: {
-      'no-console': 'off',
-      'pbs-kids/var-length': 'off',
-    },
-  };
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+    mocha: true,
+  },
+  extends: '@pbs/eslint-config-pbs-kids',
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
+  rules: {
+    'no-console': 'off',
+    'pbs-kids/var-length': 'off',
+  },
+};
